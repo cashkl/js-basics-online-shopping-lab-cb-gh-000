3ddaa69a0ecb0dd9666854712ba8bdd02cebc886
Murat Celik <cashkl@yandex.com>
2018-05-01 18:13:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.push({item,Math.floor(Math.random()*51)})
+ item.push({item,Math.floor(Math.random()*51)})
  return `${item[item.length-1]} has been added to your cart.`
 }
 
